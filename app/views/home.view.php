10242036d804816b93506b1fa589acdee9caea13
Cyril Muchemi <cyrilmuchemi@gmail.com>
2025-10-05 13:15:16 +0300

    Link all the necessary assets

--- app/views/home.view.php
+++ app/views/home.view.php
@@ -8,14 +8,8 @@
         <meta content="" name="keywords">
         <meta content="" name="description">
 
-        <!-- Google Web Fonts -->
-        <link rel="preconnect" href="<?=ROOT?>/assets/https://fonts.googleapis.com">
-        <link rel="preconnect" href="<?=ROOT?>/assets/https://fonts.gstatic.com" crossorigin>
-        <link href="<?=ROOT?>/assets/https://fonts.googleapis.com/css2?family=Great+Vibes&family=Open+Sans:wght@400;500;600&family=Petit+Formal+Script&display=swap" rel="stylesheet"> 
-
         <!-- Icon Font Stylesheet -->
-        <link rel="stylesheet" href="<?=ROOT?>/assets/https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
-        <link href="<?=ROOT?>/assets/https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+        <link rel="stylesheet" href="<?=ROOT?>/assets/fontawesome/css/all.min.css">
 
         <!-- Libraries Stylesheet -->
         <link href="<?=ROOT?>/assets/lib/animate/animate.min.css" rel="stylesheet">
@@ -28,7 +22,20 @@
         <!-- Template Stylesheet -->
         <link href="<?=ROOT?>/assets/css/style.css" rel="stylesheet">
     </head>
-
+    <style>       
+        @font-face {
+            font-family: 'Great Vibes';
+            src: url('<?=ROOT?>/assets/project-fonts/Great_Vibes/GreatVibes-Regular.ttf') format('woff2');
+        }
+        @font-face {
+            font-family: 'Open Sans';
+            src: url('<?=ROOT?>/assets/project-fonts/Open_Sans/OpenSans-Italic-VariableFont_wdth,wght.ttf') format('woff2');
+        }
+       @font-face {
+            font-family: 'Petit Formal Script';
+            src: url('<?=ROOT?>/assets/project-fonts/Petit_Formal_Script/PetitFormalScript-Regular.ttf') format('woff2');
+        }
+    </style>
     <body data-bs-spy="scroll" data-bs-target="#navBar" id="weddingHome">
 
         <!-- Spinner Start -->
@@ -986,13 +993,6 @@
                             </p>
                         </div>
                     </div>
-                    <div class="col-12 wow fadeIn" data-wow-delay="0.2s">
-                        <div class="border-secondary" style="border-style: double;">
-                            <iframe class="w-100" 
-                            style="height: 450px; margin-bottom: -6px;" src="<?=ROOT?>/assets/https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387191.33750346623!2d-73.97968099999999!3d40.6974881!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1694259649153!5m2!1sen!2sbd" 
-                            loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                        </div>
-                    </div>
                 </div>
             </div>
             </div>
@@ -1074,8 +1074,8 @@
 
         
     <!-- JavaScript Libraries -->
-    <script src="<?=ROOT?>/assets/https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-    <script src="<?=ROOT?>/assets/https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="<?=ROOT?>/assets/js/jquery.js"></script>
+    <script src="<?=ROOT?>/assets/bootstrap-5.3.8-dist/js/bootstrap.bundle.min.js"></script>
     <script src="<?=ROOT?>/assets/lib/wow/wow.min.js"></script>
     <script src="<?=ROOT?>/assets/lib/easing/easing.min.js"></script>
     <script src="<?=ROOT?>/assets/lib/waypoints/waypoints.min.js"></script>
@@ -1083,7 +1083,7 @@
     
 
     <!-- Template Javascript -->
-    <script src="js/main.js"></script>
+    <script src="<?=ROOT?>/assets/js/main.js"></script>
     </body>
 
 </html>
