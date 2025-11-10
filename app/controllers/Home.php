6421458b7e8d902735ccd944c71595be4e475251
Cyril Muchemi <cyrilmuchemi@gmail.com>
2025-11-10 10:49:21 +0300

    Change hero images

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -5,6 +5,9 @@ class Home extends Controller
     public function index(){
         $contact = new Contact_Model;
         $data['social_links'] = $contact->first(['id'=>1]);
+
+        $gallery = new Gallery_Model;
+        $data['gallery'] = $gallery->findAll();
         
         $this->view('home', $data);
     }
